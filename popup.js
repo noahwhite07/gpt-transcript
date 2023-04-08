ed1af4fb7ddc6a9ab3617734aa6a9d3a1a83e68a
Noah White <91574273+noahwhite07@users.noreply.github.com>
2023-04-08 13:19:51 -0400

    Export transcript as text file with labels

--- popup.js
+++ popup.js
@@ -1,13 +1,19 @@
 console.log("This is a popup!");
 
 document.getElementById("readHtmlButton").addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "readHtml" }, (response) => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-        } else {
-          console.log(response.html);
-        }
-      });
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const port = chrome.tabs.connect(tabs[0].id, { name: "popupToContent" });
+    port.postMessage({ action: "readHtml" });
+
+    port.onMessage.addListener((response) => {
+      console.log("Response from content.js");
+      console.log(response.html);
+    });
+
+    port.onDisconnect.addListener(() => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+      }
     });
   });
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,36 +1,48 @@
 console.log("I'm a content script.");
 
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     if (request.action === "readHtml") {
-//       const html = document.documentElement.innerHTML;
-//       sendResponse({ html: html });
-//     }
-//   });
-  
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     if (request.action === "readHtml") {
-//       const className = "min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap";
-//       const elements = Array.from(document.getElementsByClassName(className));
-  
-//       const innerHtmlList = [];
-//       elements.forEach((element) => {
-//         innerHtmlList.push(element.innerHTML);
-//       });
-  
-//       sendResponse({ html: innerHtmlList.join('\n\n') });
-//     }
-//   });
-  
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-if (request.action === "readHtml") {
-    const className = "min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap";
-    const elements = Array.from(document.getElementsByClassName(className));
+chrome.runtime.onConnect.addListener((port) => {
+    if (port.name === "popupToContent") {
+        port.onMessage.addListener((request) => {
 
-    const textList = [];
-    elements.forEach((element) => {
-    textList.push(element.innerText.trim());
-    });
+            console.log("Message from popup.js");
+            if (request.action === "readHtml") {
+                const user1ClassName = "group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 dark:bg-gray-800";
+                const user2ClassName = "group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 bg-gray-50 dark:bg-[#444654]";
 
-    sendResponse({ html: textList.join('\n\n') });
-}
+                const user1OuterDivs = Array.from(document.getElementsByClassName(user1ClassName));
+                const user2OuterDivs = Array.from(document.getElementsByClassName(user2ClassName));
+
+                const extractInnerText = (element) => {
+                    const innerDiv = element.querySelector('[class="min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap"]');
+                    return innerDiv ? innerDiv.innerText.trim() : '';
+                };
+
+                const allOuterDivs = document.querySelectorAll('[class="group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 dark:bg-gray-800"], [class="group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 bg-gray-50 dark:bg-[#444654]"]');
+
+                const messages = Array.from(allOuterDivs).map((element) => {
+                    const user = element.classList.contains('dark:bg-gray-800') ? "Me" : "GPT";
+                    return { user, text: extractInnerText(element) };
+                });
+
+                const formattedMessages = messages.map((message) => `${message.user}: "${message.text}"`).join('\n\n');
+
+                downloadTextFile('transcript.txt', formattedMessages);
+                port.postMessage({ html: formattedMessages });
+            }
+        });
+    }
 });
+
+function downloadTextFile(filename, text) {
+    const blob = new Blob([text], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+  
+    const downloadLink = document.createElement('a');
+    downloadLink.href = url;
+    downloadLink.download = filename;
+  
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+    URL.revokeObjectURL(url);
+  }
