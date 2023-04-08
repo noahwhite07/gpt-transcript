3b1d999a1617ffeee85281fe43be3e95e670de94
Noah White <91574273+noahwhite07@users.noreply.github.com>
2023-04-08 19:15:33 -0400

    Force-load content script (resolves #1)

--- popup.js
+++ popup.js
@@ -3,25 +3,42 @@ console.log("This is a popup!");
 
 // Add a click event listener for the "Read HTML" button
 document.getElementById("readHtmlButton").addEventListener("click", () => {
+  
   // Query the active tab in the current window
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    // Establish a connection to the content script in the active tab
-    const port = chrome.tabs.connect(tabs[0].id, { name: "popupToContent" });
 
-    // Send a message to the content script with the "exportTranscript" action
-    port.postMessage({ action: "exportTranscript" });
+    // Inject content script when the popup is opened
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tabs[0].id },
+        files: ['scripts/content.js'],
+      },
+      () => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError.message);
+        } else {
+          console.log('Content script injected successfully.');
 
-    // Listen for messages from the content script
-    port.onMessage.addListener((response) => {
-      console.log("Response from content.js");
-      console.log(response.html);
-    });
+          // Establish a connection to the content script in the active tab
+          const port = chrome.tabs.connect(tabs[0].id, { name: "popupToContent" });
 
-    // Handle disconnect events and log any errors
-    port.onDisconnect.addListener(() => {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError.message);
+          // Send a message to the content script with the "exportTranscript" action
+          port.postMessage({ action: "exportTranscript" });
+
+          // Listen for messages from the content script
+          port.onMessage.addListener((response) => {
+            console.log("Response from content.js");
+            console.log(response.html);
+          });
+
+          // Handle disconnect events and log any errors
+          port.onDisconnect.addListener(() => {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError.message);
+            }
+          });
+        }
       }
-    });
+    );
   });
 });
